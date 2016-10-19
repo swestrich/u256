4b540efe1e5193d0f589eea712deec0ea3d41287
Samuel Westrich <samuel.westrich@gmail.com>
2016-10-19 23:01:11 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var u256 = require('../');
+var expect    = require("chai").expect;
+
+describe("u256 based arithmatic", function() {
+  describe("creation", function() {
+    it("string", function() {
+      expect(new u256("123").u32).to.deep.equal([0,0,0,0,0,0,0,123]);
+    });
+    it("empty string", function() {
+      expect(new u256("").u32).to.deep.equal([0,0,0,0,0,0,0,0]);
+    });
+  });
+});
